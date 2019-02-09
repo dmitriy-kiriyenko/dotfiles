9c2210eaf39472c46dd27e885844d917652a0922
dmitriy-kiriyenko <dmitriy.kiriyenko@gmail.com>
2019-02-09 12:46:11 +0300

    Merge conflict resolution

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -61,6 +61,27 @@ battery_status() {
   fi
 }
 
+ruby_version() {
+  if (( $+commands[rbenv] ))
+  then
+    echo "$(rbenv version | awk '{print $1}')"
+  fi
+
+  if (( $+commands[rvm-prompt] ))
+  then
+    echo "$(rvm-prompt | awk '{print $1}')"
+  fi
+}
+
+rb_prompt() {
+  if ! [[ -z "$(ruby_version)" ]]
+  then
+    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
+  else
+    echo ""
+  fi
+}
+
 function collapse_pwd {
     echo $(pwd | sed -e "s,^$HOME,~,")
 }
